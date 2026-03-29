057b206713129dbb860b95f7cef398a0afebd7ea
Vikas Mantute <mantutevikas@yahoo.in>
2026-03-29 23:51:10 +0530

    fix: esbuild + external_node_modules for ejs and dynamic packages

--- consumer-app/netlify/functions/server.js
+++ consumer-app/netlify/functions/server.js
@@ -1,3 +1,7 @@
+// Explicit requires so esbuild registers these as external deps
+require('ejs');
+require('bcryptjs');
+
 const serverless = require('serverless-http');
 const app = require('../../app');
 
